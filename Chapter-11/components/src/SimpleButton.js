08482a1a1591341a79e00b33c8da61c637df582c
ndminh1307js <ndminh1307js@gmail.com>
2020-06-13 18:57:23 +0700

    Adding State Data

--- Chapter-11/components/src/SimpleButton.js
+++ Chapter-11/components/src/SimpleButton.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export class SimpleButton extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			counter: 0,
+			hasButtonBeenClicked: false,
+		};
+	}
 	render() {
 		return (
 			<button
@@ -9,7 +17,8 @@ export class SimpleButton extends Component {
 				disabled={
 					this.props.disabled === 'true' || this.props.disabled === true
 				}>
-				{this.props.text}
+				{this.props.text} {this.state.counter}
+				{this.state.hasButtonBeenClicked && <div>Button Clicked!</div>}
 			</button>
 		);
 	}
