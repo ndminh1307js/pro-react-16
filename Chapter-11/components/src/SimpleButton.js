12533e172a29ac75aa1a0267e7a0be18ed174678
ndminh1307js <ndminh1307js@gmail.com>
2020-06-13 19:53:03 +0700

    Avoiding Missing Multiple Updates Pitfall

--- Chapter-11/components/src/SimpleButton.js
+++ Chapter-11/components/src/SimpleButton.js
@@ -24,8 +24,10 @@ export class SimpleButton extends Component {
 	}
 
 	handleClick = () => {
-		this.setState({ counter: this.state.counter + 1 },
-			() => this.setState({ hasButtonBeenClicked: this.state.counter > 0 }));
+		for (let i = 0; i < 5; i++) {
+			this.setState((state, props) => { return { counter: state.counter + 1 } });
+		}
+		this.setState({ hasButtonBeenClicked: true })
 		this.props.callback();
 	};
 }
