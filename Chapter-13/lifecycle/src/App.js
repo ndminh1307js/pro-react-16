37ba291ada90dae7e1750d7d17a9ab7b4e021630
ndminh1307js <ndminh1307js@gmail.com>
2020-06-15 05:43:43 +0700

    Setting State Data from Prop Values

--- Chapter-13/lifecycle/src/App.js
+++ Chapter-13/lifecycle/src/App.js
@@ -1,45 +1,35 @@
 import React, { Component } from 'react';
-import { Message } from './Message';
-// import { List } from './List';
-// import { ExternalCounter } from './ExternalCounter';
-// import { HooksMessage } from './HooksMessage';
+// import { Message } from './Message';
+import { DirectionDisplay } from './DirectionDisplay';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			counterLeft: 0,
-			counterRight: 0,
+			counter: 100,
 		};
 	}
 
-	incrementCounter = counter => {
-		if (counter === 'left') {
-			this.setState({ counterLeft: this.state.counterLeft + 1 });
-		} else {
-			this.setState({ counterRight: this.state.counterRight + 1 });
-		}
+	changeCounter = val => {
+		this.setState({ counter: this.state.counter + val });
 	};
 
 	render() {
 		console.log('Render App Component');
 		return (
 			<div className='container text-center'>
-				<div className='row p-2'>
-					<div className='col-6'>
-						<Message
-							message={`Counter: ${this.state.counterLeft}`}
-							callback={() => this.incrementCounter('left')}
-							text='Increment Counter Left'
-						/>
-					</div>
-					<div className='col-6'>
-						<Message
-							message={`Counter: ${this.state.counterRight}`}
-							callback={() => this.incrementCounter('right')}
-							text='Increment Counter Right'
-						/>
-					</div>
+				<DirectionDisplay value={this.state.counter} />
+				<div className='text-center'>
+					<button
+						className='btn btn-success m-1'
+						onClick={() => this.changeCounter(1)}>
+						Go Up
+					</button>
+					<button
+						className='btn btn-danger m-1'
+						onClick={() => this.changeCounter(-1)}>
+						Go Down
+					</button>
 				</div>
 			</div>
 		);
