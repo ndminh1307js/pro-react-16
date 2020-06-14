038552d6118c1159faf29e35569f24b947beea3f
ndminh1307js <ndminh1307js@gmail.com>
2020-06-14 10:17:35 +0700

    Understanding React Events

---
+++ Chapter-12/reactevents/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Ready',
+      counter: 0,
+      theme: 'secondary'
+    };
+  }
+
+  handleEvent = (event, newTheme) => {
+    event.persist();
+    this.setState({
+      counter: this.state.counter + 1,
+      theme: newTheme
+    },
+      () => this.setState({ message: `${event.type}: ${this.state.counter}` }));
+  }
+
+  toggleCheckbox = (event) => {
+    if (this.state.counter === 0) {
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    return <div className='m-2'>
+      <div className="form-check">
+        <input type="checkbox" className="form-check-input"
+          onClick={this.toggleCheckbox} />
+        <label>This is a checkbox</label>
+      </div>
+      <div className="h4 bg-primary text-center text-white p-2">
+        {this.state.message}
+      </div>
+      <div className="text-center">
+        <button className="btn btn-primary"
+          onClick={e => this.handleEvent(e, 'primary')}>
+          Normal
+        </button>
+        <button className="btn btn-danger m-1"
+          onClick={e => this.handleEvent(e, 'danger')}>
+          Danger
+        </button>
+      </div>
+    </div>
+  }
+}
