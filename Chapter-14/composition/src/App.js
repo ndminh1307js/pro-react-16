818eff6b831540d2c660c06707e96a0ae2d623d0
ndminh1307js <ndminh1307js@gmail.com>
2020-06-15 20:59:03 +0700

    Using Render Props

--- Chapter-14/composition/src/App.js
+++ Chapter-14/composition/src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { SortedList } from './SortedList';
 import { GeneralList } from './GeneralList';
-import { ProController } from './ProController';
-import { LogToConsole } from './LogToConsole';
-
-const ProList = ProController(
-	LogToConsole(SortedList, 'Sorted List', true, true, true),
-);
+import { ProFeature } from './ProFeature';
+// import { ProController } from './ProController';
+// import { LogToConsole } from './LogToConsole';
 
 export default class App extends Component {
 	constructor(props) {
@@ -14,24 +11,39 @@ export default class App extends Component {
 		this.state = {
 			names: ['Zoe', 'Bob', 'Alice', 'Dora', 'Joe'],
 			cities: ['London', 'New York', 'Madrid', 'Paris', 'Milan'],
+			proMode: false,
 		};
 	}
+
+	toggleProMode = () => {
+		this.setState({ proMode: !this.state.proMode });
+	};
 
 	render() {
 		return (
 			<div className='container-fluid'>
 				<div className='row'>
-					<div className='col-3'>
+					<div className='col-12 text-center-p-2'>
+						<div className='form-check'>
+							<input
+								type='checkbox'
+								className='form-check-input'
+								value={this.state.proMode}
+								onChange={this.toggleProMode}
+							/>
+							<label className='form-check-label'>Pro Mode</label>
+						</div>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col-6'>
 						<GeneralList list={this.state.names} theme='primary' />
 					</div>
-					<div className='col-3'>
-						<ProList list={this.state.names} />
-					</div>
-					<div className='col-3'>
-						<GeneralList list={this.state.cities} theme='secondary' />
-					</div>
-					<div className='col-3'>
-						<ProList list={this.state.cities} />
+					<div className='col-6'>
+						<ProFeature
+							pro={this.state.proMode}
+							render={() => <SortedList list={this.state.names} />}
+						/>
 					</div>
 				</div>
 			</div>
--- Chapter-14/composition/src/ProFeature.js
+++ Chapter-14/composition/src/ProFeature.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-export function ProFeature(FeatureComponent) {
-	return function (props) {
-		if (props.pro) {
-			let { pro, ...childProps } = props;
-			return <FeatureComponent {...childProps} />;
-		} else {
-			return (
-				<h5 className='bg-warning text-center text-white'>
-					This is Pro Feature
-				</h5>
-			);
-		}
-	};
+export function ProFeature(props) {
+	if (props.pro) {
+		return props.render();
+	} else {
+		return (
+			<h5 className='bg-warning text-center text-white'>This is Pro Feature</h5>
+		);
+	}
 }
