d6020aaccb79938eac72559989a9b170575ac47d
ndminh1307js <ndminh1307js@gmail.com>
2020-06-16 17:09:33 +0700

    Creating Refs using Callback Functions

--- Chapter-16/refs/src/Editor.js
+++ Chapter-16/refs/src/Editor.js
@@ -3,38 +3,43 @@ import React, { Component } from 'react';
 export class Editor extends Component {
   constructor(props) {
     super(props);
-    this.nameRef = React.createRef();
-    this.categoryRef = React.createRef();
-    this.priceRef = React.createRef();
+    this.formElements = {
+      name: { label: 'Name', name: 'name' },
+      category: { label: 'Category', name: 'category' },
+      price: { label: 'Price', name: 'price' }
+    }
+  }
+
+  setElement = (element) => {
+    if (element !== null) {
+      this.formElements[element.name].element = element;
+    }
   }
 
   handleAdd = () => {
-    this.props.callback(this.state);
-    this.setState({ name: '', category: '', price: '' }, () => this.nameRef.current.focus());
+    let data = {};
+    Object.values(this.formElements)
+      .forEach(v => {
+        data[v.element.name] = v.element.value;
+        v.element.value = '';
+      })
+    this.props.callback(data);
+    this.formElements.name.element.focus();
   }
 
   render() {
     return <React.Fragment>
-      <div className="form-group p-2">
-        <label>Name</label>
-        <input className='form-control' name='name'
-          value={this.state.name} onChange={this.handleChange}
-          autoFocus={true} ref={this.nameRef} />
-      </div>
-      <div className="form-group p-2">
-        <label>Category</label>
-        <input className='form-control' name='category'
-          value={this.state.category} onChange={this.handleChange}
-          ref={this.categoryRef} />
-      </div>
-      <div className="form-group p-2">
-        <label>Price</label>
-        <input type='number' className='form-control' name='price'
-          value={this.state.price} onChange={this.handleChange}
-          ref={this.priceRef} />
-      </div>
+      {Object.values(this.formElements).map(elem =>
+        <div className="form-group p-2">
+          <label>{elem.label}</label>
+          <input className='form-control' name={elem.name}
+            onChange={this.handleChange} ref={this.setElement} />
+        </div>
+      )}
+
       <div class='text-center p-2'>
-        <button className="btn btn-primary btn-block btn-big" onClick={this.handleAdd}>
+        <button className="btn btn-primary btn-block btn-big"
+          onClick={this.handleAdd}>
           Add
         </button>
       </div>
