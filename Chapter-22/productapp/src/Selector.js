df53fee661d42c800ce1e4829af73a8091c5580e
ndminh1307js <ndminh1307js@gmail.com>
2020-06-21 08:23:01 +0700

    Accessing Routing Data using Higher-Order Component

--- Chapter-22/productapp/src/Selector.js
+++ Chapter-22/productapp/src/Selector.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, NavLink, Route, Switch, Redirect }
+import { BrowserRouter as Router, Route, Switch, Redirect, withRouter }
   from 'react-router-dom';
 import { ProductDisplay } from './ProductDisplay';
 import { SupplierDisplay } from './SupplierDisplay';
 import { RouteInfo } from './routing/RouteInfo';
 import { ToggleLink } from './routing/ToggleLink';
+
+const RouteInfoHOC = withRouter(RouteInfo);
 
 export class Selector extends Component {
 
@@ -22,6 +24,7 @@ export class Selector extends Component {
             <ToggleLink to='/info'>All Info</ToggleLink>
           </div>
           <div className="col-10">
+            <RouteInfoHOC />
             <Switch>
               <Route path='/products' component={ProductDisplay} />
               <Route path='/suppliers' component={SupplierDisplay} />
