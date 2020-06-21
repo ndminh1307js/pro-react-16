26de9f52ee0cb26c61f33a8d217849afa28e8a92
ndminh1307js <ndminh1307js@gmail.com>
2020-06-21 08:53:19 +0700

    Presenting a Custom Navigation Prompt

--- Chapter-22/productapp/src/Selector.js
+++ Chapter-22/productapp/src/Selector.js
@@ -5,15 +5,36 @@ import { ProductDisplay } from './ProductDisplay';
 import { SupplierDisplay } from './SupplierDisplay';
 import { RouteInfo } from './routing/RouteInfo';
 import { ToggleLink } from './routing/ToggleLink';
+import { CustomPrompt } from './routing/CustomPrompt';
 
 const RouteInfoHOC = withRouter(RouteInfo);
 
 export class Selector extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPrompt: false,
+      message: '',
+      callback: () => { }
+    }
+  }
+
+  customGetUserConfirmation = (message, navCallback) => {
+    this.setState({
+      showPrompt: true,
+      message,
+      callback: (allow) => {
+        navCallback(allow);
+        this.setState({ showPrompt: false })
+      }
+    })
+  }
+
   renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{msg}</h5>
 
   render() {
-    return <Router>
+    return <Router getUserConfirmation={this.customGetUserConfirmation}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-2">
@@ -24,6 +45,9 @@ export class Selector extends Component {
             <ToggleLink to='/info'>All Info</ToggleLink>
           </div>
           <div className="col-10">
+            <CustomPrompt show={this.state.showPrompt}
+              message={this.state.message}
+              callback={this.state.callback} />
             <Prompt message={location => `Do you want to navigate to ${location.pathname}`} />
             <RouteInfoHOC />
             <Switch>
