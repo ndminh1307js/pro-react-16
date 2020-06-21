03c8af2caa191080e331fadccfc3b911afe5cbbc
ndminh1307js <ndminh1307js@gmail.com>
2020-06-21 09:01:09 +0700

    Using Routes Programmatically

--- Chapter-22/productapp/src/Selector.js
+++ Chapter-22/productapp/src/Selector.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect, withRouter, Prompt }
   from 'react-router-dom';
-import { ProductDisplay } from './ProductDisplay';
-import { SupplierDisplay } from './SupplierDisplay';
-import { RouteInfo } from './routing/RouteInfo';
 import { ToggleLink } from './routing/ToggleLink';
 import { CustomPrompt } from './routing/CustomPrompt';
-
-const RouteInfoHOC = withRouter(RouteInfo);
 
 export class Selector extends Component {
 
@@ -34,27 +29,25 @@ export class Selector extends Component {
   renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{msg}</h5>
 
   render() {
+    const routes = React.Children.map(this.props.children, child => ({
+      component: child,
+      name: child.props.name,
+      url: `/${child.props.name.toLowerCase()}`
+    }))
     return <Router getUserConfirmation={this.customGetUserConfirmation}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <ToggleLink to='/products'>Products</ToggleLink>
-            <ToggleLink to='/Suppliers'>Suppliers</ToggleLink>
-            <ToggleLink to='/info/match'>Match Info</ToggleLink>
-            <ToggleLink to='/info/location'>Location Info</ToggleLink>
-            <ToggleLink to='/info'>All Info</ToggleLink>
+            {routes.map(r => <ToggleLink to={r.url} key={r.url}>{r.name}</ToggleLink>)}
           </div>
           <div className="col-10">
             <CustomPrompt show={this.state.showPrompt}
               message={this.state.message}
               callback={this.state.callback} />
             <Prompt message={location => `Do you want to navigate to ${location.pathname}`} />
-            <RouteInfoHOC />
             <Switch>
-              <Route path='/products' component={ProductDisplay} />
-              <Route path='/suppliers' component={SupplierDisplay} />
-              <Route path='/info/:datatype?' component={RouteInfo} />
-              <Redirect to='/products' />
+              {routes.map(r => <Route key={r.url} path={r.url} render={() => r.component} />)}
+              <Redirect to={routes[0].url} />
             </Switch>
           </div>
         </div>
